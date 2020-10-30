24337ba85a33f5389788e0acf502cf165f73aa3b
ZhangKaiYuan <1570171143@qq.com>
2020-10-30 11:09:29 +0800

    new project blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import App from 'next/app'
+import 'antd/dist/antd.css'
+import '../styles/pages/comm.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
-export default MyApp
+
+export default App
